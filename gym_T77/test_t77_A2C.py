f3cf69a758d7147c2e62f5d5576008cca4acb054
Han-JD <han.ja@husky.neu.edu>
2019-10-07 12:32:30 -0400

    gym_T77 Stable_baseline A2C tested via Jupyter notebook.

---
+++ gym_T77/test_t77_A2C.py
@@ -0,0 +1,26 @@
+import gym
+import sys
+
+from stable_baselines.common.policies import MlpPolicy
+from stable_baselines.common.vec_env import SubprocVecEnv
+from stable_baselines import A2C
+
+sys.path.append(r"C:\Users\woare\OneDrive\Documents\RL\gym-T77")
+
+# multiprocess environment
+n_cpu = 1
+env = SubprocVecEnv([lambda: gym.make('gym_t77:t77-v0') for i in range(n_cpu)])
+
+model = A2C(MlpPolicy, env, verbose=1)
+model.learn(total_timesteps=25000)
+model.save("a2c_t77")
+
+del model  # remove to demonstrate saving and loading
+
+model = A2C.load("a2c_t77")
+
+obs = env.reset()
+while True:
+    action, _states = model.predict(obs)
+    obs, rewards, dones, info = env.step(action)
+    env.render()
